9d12eebddf17e49a9a30cd5d59e03f0e009be3c1
Simonistgren <37584355+Simonistgren@users.noreply.github.com>
2018-03-22 11:26:48 +0100

    Add files via upload

---
+++ Grupp1Kandidaten/src/Bluetooth/Transmitter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bluetooth;
+
+
+import java.io.*;
+import javax.microedition.io.*;
+import javax.bluetooth.*;
+public class Transmitter {
+
+ public static void main(String args[]) {
+try {
+StreamConnection anslutning = (StreamConnection) 
+        //20:14:10:14:90:18
+        Connector.open("btspp://201410149018:1");
+PrintStream bluetooth_ut = new PrintStream(anslutning.openOutputStream());
+  
+bluetooth_ut.println("Dressed for sucess"); 
+//Thread.sleep(500);
+anslutning.close();
+} 
+catch 
+(Exception e) {  System.out.print(e.toString());   }
+    }
+}
