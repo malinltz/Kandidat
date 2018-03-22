b8eb6c676075f37081472150d28dc1564039d17d
ricbl954 <37584372+ricbl954@users.noreply.github.com>
2018-03-22 11:44:38 +0100

    Add files via upload

---
+++ Test/DataStore.java
@@ -0,0 +1,106 @@
+package Grupp1Kandidaten;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class DataStore {
+
+    String fileName = null;
+    int nodes;
+    int arcs;
+    double[] nodeX;
+    double[] nodeY;
+    int[] arcStart;
+    int[] arcEnd;
+    int[] arcCost;
+    boolean networkRead;
+    boolean updateUIflag;
+
+    double robotX;
+    double robotY;
+    int[] arcColor;
+
+    public DataStore() {
+        // Initialize the datastore with fixed size arrays for storing the network data
+        nodes = 0;
+        arcs = 0;
+        nodeX = new double[1000];
+        nodeY = new double[1000];
+        arcStart = new int[1000];
+        arcEnd = new int[1000];
+        arcCost = new int[1000];
+        arcColor = new int[1000];
+
+        networkRead = false;
+        updateUIflag = false;
+
+    }
+
+    public void setFileName(String newFileName) {
+        this.fileName = newFileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void readNet() {
+        String line;
+
+        if (fileName == null) {
+            System.err.println("No file name set. Data read aborted.");
+            return;
+        }
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file, "iso-8859-1");
+            String[] sline;
+
+            // Read number of nodes
+            line = (scanner.nextLine());
+            nodes = Integer.parseInt(line.trim());
+            line = scanner.nextLine();
+            arcs = Integer.parseInt(line.trim());
+
+            // Debug printout: network size data
+            System.out.println("Nodes: " + nodes);
+            System.out.println("Arcs: " + arcs);
+
+            // Read nodes as number, x, y
+            for (int i = 0; i < nodes; i++) {
+                line = scanner.nextLine();
+                //split space separated data on line
+                sline = line.split(" ");
+                nodeX[i] = Double.parseDouble(sline[1].trim());
+                nodeY[i] = Double.parseDouble(sline[2].trim());
+            }
+
+            // Debug printout: print data for node 1
+            System.out.println("Node 1: " + nodeX[0] + " " + nodeY[0]);
+
+            // Read arc list as start node number, end node number
+            for (int i = 0; i < arcs; i++) {
+                line = scanner.nextLine();
+                //split space separated data on line
+                sline = line.split(" ");
+                arcStart[i] = Integer.parseInt(sline[0].trim());
+                arcEnd[i] = Integer.parseInt(sline[1].trim());
+                arcCost[i] = Integer.parseInt(sline[2].trim());
+            }
+
+            networkRead = true;  // Indicate that all network data is in place in the DataStore
+            updateUIflag = true;
+
+            robotX = nodeX[70];
+            robotY = nodeY[70];
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ Test/MapPanel.java
@@ -0,0 +1,83 @@
+package Grupp1Kandidaten;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.lang.Math.*;
+
+/**
+ *
+ */
+public class MapPanel extends JPanel {
+
+    DataStore ds;
+
+    MapPanel(DataStore ds) {
+        this.ds = ds;
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        final Color LIGHT_COLOR = new Color(150, 150, 150);
+        final Color DARK_COLOR = new Color(0, 0, 0);
+        final Color RED_COLOR = new Color(255, 0, 0);
+        int x, y;
+        int x1, y1;
+        int x2, y2;
+
+        final int circlesize = 7;
+        final int ysize = 350;
+        final int xsize = 700;
+
+        if (ds.networkRead == true) { // Only try to plot is data has been properly read from file
+
+            // Compute scale factor in order to keep the map in proportion when the window is resized
+            int height = getHeight();
+            int width = getWidth();
+            double xscale = 1.0 * width / xsize;
+            double yscale = 1.0 * height / ysize;
+
+            g.setColor(DARK_COLOR);
+
+            // Draw nodes as circles
+            for (int i = 0; i < ds.nodes; i++) {
+                x = (int) (ds.nodeX[i] * xscale);
+                y = (int) (ds.nodeY[i] * yscale);
+
+                g.fillOval(x - (circlesize / 2), height - y - circlesize / 2, circlesize, circlesize);
+            }
+
+            // Draw arcs
+            for (int i = 0; i < ds.arcs; i++) {
+                x1 = (int) (ds.nodeX[ds.arcStart[i] - 1] * xscale);
+                y1 = (int) (ds.nodeY[ds.arcStart[i] - 1] * yscale);
+                x2 = (int) (ds.nodeX[ds.arcEnd[i] - 1] * xscale);
+                y2 = (int) (ds.nodeY[ds.arcEnd[i] - 1] * yscale);
+                int dist;
+                dist = (int) Math.round(Math.hypot((ds.nodeX[ds.arcStart[i] - 1] - ds.nodeX[ds.arcEnd[i] - 1]), (ds.nodeY[ds.arcStart[i] - 1] - ds.nodeY[ds.arcEnd[i] - 1])));
+             
+
+                g.drawString(String.valueOf(dist), (x1 + x2) / 2, (height - y1 + height - y2) / 2);
+                g.setFont(new Font("TimesRoman", Font.PLAIN, 10));
+
+                System.out.println("Arc " + i + ": " + ds.arcStart[i] + " " + ds.arcEnd[i]);
+                
+                
+                // System.out.println("Arc "+i+": "+ds.arcStart[i]+" "+ds.arcEnd[i]);
+                if (ds.arcColor[i] == 1) {
+                    g.setColor(RED_COLOR);
+                } else {
+                    g.setColor(DARK_COLOR);
+                }
+                g.drawLine(x1, height - y1, x2, height - y2);
+            }
+
+            int x3 = (int) (ds.robotX * yscale);
+            int y3 = (int) (ds.robotY * xscale);
+            g.setColor(RED_COLOR);
+            g.drawOval(x3 - ((circlesize + 10)/ 2), height - y3 - ((circlesize + 10)) / 2, circlesize + 10, circlesize + 10);
+ 
+            }
+    }// end paintComponent
+}
