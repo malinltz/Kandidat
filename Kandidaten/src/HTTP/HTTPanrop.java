95b6bb49de799ef09e8d4a38956de40108e31c08
malinltz <malinltz62@gmail.com>
2018-03-28 13:49:30 +0200

    http

--- Kandidaten/src/HTTP/HTTPanrop.java
+++ Kandidaten/src/HTTP/HTTPanrop.java
@@ -19,13 +19,14 @@ public class HTTPanrop {
     public static void main(String[] args) {
         try {
             HTTPanrop http = new HTTPanrop();
-            String url = "https://liu.se";
+            String url = "http://tnk111.n7.se/putmessage.php?\n"
+                    + "groupid=1&messagetype=2&message=test";
             URL urlobjekt = new URL(url);
             HttpURLConnection anslutning = (HttpURLConnection) urlobjekt.openConnection();
             System.out.println("\nAnropar: " + url);
             int mottagen_status = anslutning.getResponseCode();
             System.out.println("Statuskod: " + mottagen_status);
-           
+
             BufferedReader inkommande = new BufferedReader(new InputStreamReader(anslutning.getInputStream()));
             String inkommande_text;
             StringBuffer inkommande_samlat = new StringBuffer();
