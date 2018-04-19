9f1138be5a977b298374223130c4e85dd7f15e55
Torstis <37584235+Torstis@users.noreply.github.com>
2018-04-19 16:22:15 +0200

    hej

--- Kandidaten/src/KTS3G1/Reciver.java
+++ Kandidaten/src/KTS3G1/Reciver.java
@@ -1,15 +1,40 @@
 package KTS3G1;
 
-/*
-package Bluetooth;
 
 import java.io.*;
 import javax.microedition.io.*;
 import javax.bluetooth.*;
 
-public class Receiver {
+public class Reciver implements Runnable {
+    private ControlUI cui;
+    
+      public Reciver(ControlUI cui) {
+       
+        this.cui = cui; 
+  
 
-    public static void
+    }
+    public void run(){
+         try {
+ 
+            StreamConnectionNotifier service = (StreamConnectionNotifier) Connector.open("btspp://localhost:" + new UUID(0x1101).toString() + ";name=TNK111-test");
+
+            StreamConnection anslutning = (StreamConnection) service.acceptAndOpen();
+            InputStream bluetooth_in = anslutning.openInputStream();
+            byte buffer[] = new byte[80];
+            int antal_bytes = bluetooth_in.read(buffer);
+            String mottaget
+                    = new String(buffer,
+                            0, antal_bytes);
+            System.out.println("\n"+"Mottaget meddelande: "+ mottaget);
+            cui.appendStatus(mottaget);
+        anslutning.close();
+        } catch (IOException e) {
+            System.err.print(e.toString());
+        }
+    }
+}
+  /*  public static void
             main(String args[]) {
         try {
  
@@ -23,10 +48,10 @@ public class Receiver {
                     = new String(buffer,
                             0, antal_bytes);
             System.out.println("\n"+"Mottaget meddelande: "+ mottaget);
+            
         anslutning.close();
         } catch (IOException e) {
             System.err.print(e.toString());
         }
-    }
-}
-*/
+    }*/
+
