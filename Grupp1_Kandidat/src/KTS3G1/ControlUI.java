7c3aba1339fb28ec752e5e631b3707a01c5beb02
Simonistgren <simonistgren96@gmail.com>
2018-04-25 16:42:29 +0200

    asd

--- Grupp1_Kandidat/src/KTS3G1/ControlUI.java
+++ Grupp1_Kandidat/src/KTS3G1/ControlUI.java
@@ -545,6 +545,11 @@ public class ControlUI extends javax.swing.JFrame {
         t3.start();*/
 
 
+            /*Transceiver p1 = new Transceiver(cui); 
+        Thread t3 = new Thread(p1);
+        t3.start();*/
+
+
         } else if (startStopp.getText().equals("Stop")) {
             startStopp.setText("Start");
             startStopp.setForeground(new java.awt.Color(0, 255, 0));
