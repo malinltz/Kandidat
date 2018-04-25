48ffd6a974ae53807cc80a5d65a6e1e470cfaccc
malinltz <malinltz62@gmail.com>
2018-04-25 15:41:39 +0200

    hej

--- Grupp1_Kandidat/src/KTS3G1/HTTPextern.java
+++ Grupp1_Kandidat/src/KTS3G1/HTTPextern.java
@@ -5,10 +5,43 @@
  */
 package KTS3G1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+//import java.util.List;
+import java.io.DataOutputStream;
+//import java.util.regex.Pattern;
+
 /**
  *
  * @author malinlilliecreutz
  */
 public class HTTPextern {
     
+    public OptPlan op;
+    public DataStore ds;
+    public HTTPanrop http;
+    private int sleepTime;
+    
+
+
+public HTTPextern (HTTPanrop http){
+    
+this.http= http;
+sleepTime= 1000;
+
+}
+
+public void exprotokoll(){
+
+if ()
+    
+System.out.println("");
+System.out.println();
+System.out.println();
+
+}
+
+
 }
