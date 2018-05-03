adfeeb04d9a4c77e45a4dc35c2085647dc9e0ee9
Simonistgren <simonistgren96@gmail.com>
2018-05-03 12:58:29 +0200

    uppdate

--- Grupp1_Kandidat/src/KTS3G1/Anslutning.java
+++ Grupp1_Kandidat/src/KTS3G1/Anslutning.java
@@ -1,53 +1,87 @@
 package KTS3G1;
 import java.io.*;
 import java.lang.*;
+import java.util.concurrent.TimeUnit;
 import javax.microedition.io.*;
 import javax.bluetooth.*;
 
-public class Anslutning implements Runnable {
-String lista = "rhhrr";
-public String kommando;
+ class tradTC implements Runnable {
+    public void run() {
+        System.out.println("Startar Transceiver!");
+        Transceiver p1 = new Transceiver(); 
+    }
+}
+
+ class tradRC implements Runnable {
+    public void run() {
+        System.out.println("Startar Receiver!");
+        Receiver p2 = new Receiver(); 
+    }
+}
+
+public class Anslutning  implements Runnable {
+//********* Variabler: *********************
+String lista = "hhrr";
+public static String kommando;
+public static String inskickat;
 public static StreamConnection service;
+
 Transceiver tc;
 Receiver rc;
-    public Anslutning(){
-        try {
-         // btspp://201410149018:1
-            service = (StreamConnection) Connector.open("btspp://201410149018:1");
-
-            System.out.println("Startar Transceiver!");
-            Transceiver p1 = new Transceiver(); 
-            Thread t4 = new Thread(p1);  
-            t4.start();
-           
-            int i = 0;
-            kommando = String.valueOf(lista.charAt(i));
-            System.out.println("tc.mottaget =  " + tc.mottaget);
-            System.out.println(tc.mottaget);
-            System.out.println("Kommando = " + kommando);
-           
-            if(tc.mottaget.equals(kommando)){
-                System.out.println("Startar Receiver och avslutar Transceiver!");
-                 t4.interrupt();
-                 Receiver b2 = new Receiver();
-                 Thread t5 = new Thread(b2);
-                 t5.start();
-            }
-            else
-                System.out.println("If-satsen funkar ej!");
-            if(rc.mottaget != null){
-                i++;
-                System.out.println("Startar Transceiver och avslutar Receiver!");
-                t4.start();
-            }       
-        } catch (Exception e) {
-          System.out.print(e.toString());
-        }  
+// *****************************************
+    public Anslutning()  {
+ 
        }
+
     @Override
     public void run() {
+                try {
+         // btspp://201410149018:1
+            service = (StreamConnection) Connector.open("btspp://201410149018:1");
+            
+            tradTC t1 = new tradTC();
+            Thread b1 = new Thread(t1);
+            b1.start();
+            
+            tradRC t2 = new tradRC();
+            Thread b2 = new Thread(t2);
+            
+            kommando = String.valueOf(lista.charAt(0));
+            
+            System.out.println("Kommando = " + kommando);
+            System.out.println("Inskickat = " + inskickat);
+            TimeUnit.MILLISECONDS.sleep(180);
+            
+            int i = 0;
+            while(true){
+                
+            if (inskickat != null){
+                System.out.println("Inskickat = " + inskickat);
+                
+            if(inskickat.equals(kommando)){
+                System.out.println("Startar Receiver och avslutar Transceiver!");
+                b1.interrupt(); // Stoppar Transceiver
+                b2.start(); // Startar Receiver
+            }
+            else{
+                System.out.println("If-satsen funkar ej!");
+            }
+            if(Receiver.mottaget != null){
+                b2.interrupt(); // Stoppar Receiver
+                i++;
+                kommando = String.valueOf(lista.charAt(i));
+                b1.start(); // Startar Transceiver
+                
+                
+                System.out.println("Startar Transceiver och avslutar Receiver!");
+                
+              }    
+             }
+           }
+        } catch (Exception e) {
+          System.out.print(e.toString());
+        } 
     }
- 
 }
 
 
--- Grupp1_Kandidat/src/KTS3G1/Receiver.java
+++ Grupp1_Kandidat/src/KTS3G1/Receiver.java
@@ -8,13 +8,29 @@ import javax.microedition.io.*;
 import javax.bluetooth.*;
 
 public class Receiver implements Runnable {
-    public String mottaget = null;
-            
-    Anslutning asg;
-    
+    public static String mottaget;
+
       public Receiver() {
           
           System.out.println("Receiver startad i public Receiver run!");
+          try {
+            System.out.println("Receiver startad i public void run!");
+            StreamConnectionNotifier anslut = (StreamConnectionNotifier) Anslutning.service;
+            StreamConnection anslutning = (StreamConnection) anslut.acceptAndOpen();
+            InputStream bluetooth_in = anslutning.openInputStream();
+            byte buffer[] = new byte[80];
+            int antal_bytes = bluetooth_in.read(buffer);
+          
+            mottaget = new String(buffer,  0, antal_bytes);
+            System.out.println("\n"+"Mottaget meddelande(i rc): "+ mottaget);
+                  anslutning.close();
+                  if(mottaget != null){
+                      
+                  }
+        } catch (IOException e) {
+              System.err.print(e.toString());
+        }
+          
            /*try {
             // btspp://201410149018:1
             // StreamConnectionNotifier service = (StreamConnectionNotifier) Connector.open("btspp://localhost:" + new UUID(0x1101).toString() + ";name=TNK111-test");
@@ -41,28 +57,7 @@ public class Receiver implements Runnable {
     
     @Override
     public void run() {
-        try {
-            System.out.println("Receiver startad i public void run!");
-            StreamConnectionNotifier anslut = (StreamConnectionNotifier) asg.service;
-            StreamConnection anslutning = (StreamConnection) anslut.acceptAndOpen();
-            InputStream bluetooth_in = anslutning.openInputStream();
-            byte buffer[] = new byte[80];
-            int antal_bytes = bluetooth_in.read(buffer);
-          
-            mottaget = new String(buffer,  0, antal_bytes);
-            System.out.println("\n"+"Mottaget meddelande(i rc): "+ mottaget);
-                  anslutning.close();
-                  if(mottaget != null){
-                      
-                  }
-        } catch (IOException e) {
-              System.err.print(e.toString());
-        }
-            
-         //   cui.appendStatus(mottaget);
-
-    }
-    
+                 }
     }
 
 
