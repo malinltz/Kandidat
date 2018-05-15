3e05ab4583fe2110b1a233e12ec2c50b00c52063
Richard <ricbl954@student.liu.se>
2018-05-15 10:30:02 +0200

    extern

--- Grupp1_Kandidat/src/KTS3G1/HTTPextern.java
+++ Grupp1_Kandidat/src/KTS3G1/HTTPextern.java
@@ -4,7 +4,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package KTS3G1;
 
 import java.io.BufferedReader;
@@ -17,7 +16,6 @@ import java.io.DataOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 
 public class HTTPextern {
 
@@ -62,7 +60,6 @@ public class HTTPextern {
 
     public void exprotokoll() {
 
-        
         for (int i = 0; i < http.meddelandet; i++) {
             dummer = http.uppdrag[i].split(",");
             if (http.iD[i] == 1) {
@@ -84,100 +81,86 @@ public class HTTPextern {
         System.out.println("Grupp 1 Uppdrag" + uppdragG1);
         System.out.println("Grupp 4 Uppdrag" + uppdragG4);
         System.out.println("Grupp 5 Uppdrag" + uppdragG5);
-        
+
         ejUppdrag.clear();
-        for (int i = 0; i < 3; i++){ //Tre grupper
-            
+        for (int i = 0; i < 3; i++) { //Tre grupper
+
             if (http.iD[i] != 1) //Får felmeddelande för att ID=1 (vår grupp) är den enda som ligger uppe.
             {
-                //Jämför om fler grupper vill ha samma plats.
+                //JÄMFÖR UPPDRAGSPLATS SOM ALLA GRUPPER VILL HA
                 if (http.paxplats[i] == http.paxplats[1]) {
-                    
-                    //Kolla hur det är med kostnaderna
-                   if (Integer.parseInt(http.kostnad[i]) < Integer.parseInt(http.kostnad[0])) { 
-                        
-                       //Lägga till här att vi inte kan få den uppdragsplatsen.
-                       if (i == 0) {
-                          for (int j = 0; j < uppdragG4.size(); j++) {
-                             if (!ejUppdrag.contains(j)) {
-                                  ejUppdrag.add(String.valueOf(j));
 
-                                 }
-                                }
-                            } else if (i == 2) {
-                          for (int j = 0; j < uppdragG5.size(); j++) {
-                             if (!ejUppdrag.contains(Integer.parseInt(uppdragG5.get(j)))) {
-                                 ejUppdrag.add(uppdragG5.get(j));
+                    //JÄMFÖR KOSTNADER SOM ALLA GRUPPER HAR
+                    if (Integer.parseInt(http.kostnad[i]) < Integer.parseInt(http.kostnad[0])) {
 
-                                 }
+                        //Lägga till här att vi inte kan få den uppdragsplatsen.
+                        if (i == 0) {
+                            for (int j = 0; j < uppdragG4.size(); j++) {
+                                if (!ejUppdrag.contains(j)) {
+                                    ejUppdrag.add(String.valueOf(j));
+
                                 }
                             }
+                        } else if (i == 2) {
+                            for (int j = 0; j < uppdragG5.size(); j++) {
+                                if (!ejUppdrag.contains(Integer.parseInt(uppdragG5.get(j)))) {
+                                    ejUppdrag.add(uppdragG5.get(j));
+
+                                }
+                            }
+                        }
+                    } else if (Integer.parseInt(http.kostnad[i]) == Integer.parseInt(http.kostnad[0])) {
+
+                        //JÄMFÖR ANTAL UPPDRAG SOM EN GRUPP VILL HA
+                        if (uppdragG5.size() == uppdragG1.size() || uppdragG4.size() == uppdragG1.size()) {
+
+                            //JÄMFÖR KAPACITETEN FÖR ALLA GRUPPER HÄR
+                            
+                            
+                        } else if (uppdragG5.size() > uppdragG1.size() || uppdragG4.size() > uppdragG1.size()) {
+
+                            //Lägga till här att vi inte kan få den uppdragsplatsen.
+                            if (i == 0) {
+                                for (int j = 0; j < uppdragG4.size(); j++) {
+                                    if (!ejUppdrag.contains(j)) {
+                                        ejUppdrag.add(String.valueOf(j));
+
+                                    }
+                                }
+                            } else if (i == 2) {
+                                for (int j = 0; j < uppdragG5.size(); j++) {
+                                    if (!ejUppdrag.contains(Integer.parseInt(uppdragG5.get(j)))) {
+                                        ejUppdrag.add(uppdragG5.get(j));
+
+                                    }
+                                }
+                            }
+                        } else {
+                            platsViFick2 = http.narmstaPlats;
+                        }
+                    } else {
+                        platsViFick2 = http.narmstaPlats;
                     }
-                   else if (Integer.parseInt(http.kostnad[i]) == Integer.parseInt(http.kostnad[0])) {
-                       
-                        //Då vill vi jämföra grupp-ID
-                        if (http.iD[i] < http.iD[0]) {
-                            
-                           //Lägga till här att vi inte kan få den uppdragsplatsen.
-                       if (i == 0) {
-                          for (int j = 0; j < uppdragG4.size(); j++) {
-                             if (!ejUppdrag.contains(j)) {
-                                  ejUppdrag.add(String.valueOf(j));
-
-                                 }
-                                }
-                            } else if (i == 2) {
-                          for (int j = 0; j < uppdragG5.size(); j++) {
-                             if (!ejUppdrag.contains(uppdragG5.get(j))) {
-                                 ejUppdrag.add(uppdragG5.get(j));
-
-                                 }
-                                }
-                            }
-                        }
-                   else{
-                           platsViFick2 = http.narmstaPlats;
-                        }
-                    
-                        //LÄGGA TILL om antal uppdrag en grupp vill ha
-                        if(uppdragG5.size() == uppdragG1.size()){
-                            
-                            //Beräkna kapaciteten här
-                            
-                          }
-                        else if(uppdragG4.size() == uppdragG1.size()){
-                            
-                            //Beräkna kapaciteten här
-                            
-                        }
-                        else{
-                            
-                        }
-                  }
-                }
-                else {
+                } else {
                     platsViFick2 = http.narmstaPlats;
                 }
             }
         }
         //Gör om till ArrayList
-      List<String> wordList = Arrays.asList(http.uppdrag);  
-      
- 
+        List<String> wordList = Arrays.asList(http.uppdrag);
+
         //Här tar den bort de uppdrag som de andra grupperna fick
-         for (int k = 0; k < wordList.size(); k++) {
+        for (int k = 0; k < wordList.size(); k++) {
             for (int j = 0; j < ejUppdrag.size(); j++) {
-               if (wordList.get(k) == ejUppdrag.get(j)) {
-                   wordList.remove(k);
-           }
+                if (wordList.get(k) == ejUppdrag.get(j)) {
+                    wordList.remove(k);
+                }
+            }
         }
-     }
-         
-         //Gör om till Array String igen.
-      platsViFick1 = wordList.stream().toArray(String[]::new);
-         
-      platsViFick2 = String.join(",", platsViFick1);
-  }   
+
+        //Gör om till Array String igen.
+        platsViFick1 = wordList.stream().toArray(String[]::new);
+
+        platsViFick2 = String.join(",", platsViFick1);
+    }
 }
-
-
